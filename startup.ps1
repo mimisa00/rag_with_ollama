148c97246d98ed18aa881aa6697db830ff77f110
Shaun <mimisa00@gmail.com>
2025-07-22 10:50:19 +0800

    Update startup.ps1
    Adjusting Startup Instructions for Windows Environment

--- startup.ps1
+++ startup.ps1
@@ -4,7 +4,7 @@
 2. Install MariaDB v10.11 and create database accounts. The account credentials must match those defined in env.
 3. Initialize the database      : Execute all SQL files under the init-db directory.
 4. Set up environment variables : Copy env.example to .env and place it in the project root directory.
-5. Create working directories   : documents faiss_data logs mariadb_data models ollama
+5. Create working directories   : documents faiss_data logs models
 6. Upgrade Python pip           : pip install --upgrade pip
 7. Install Python dependencies: : pip install -r requirements.txt
 8. Start FastAPI server         : uvicorn app.app:app --host 127.0.0.1 --port 8080
